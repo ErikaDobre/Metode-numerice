da0ad08ee068ab22361d5646ad94747bcc3bc719
ErikaDobre <168623275+ErikaDobre@users.noreply.github.com>
2024-05-12 21:43:10 +0300

    Add files via upload

---
+++ exercitiul 1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,x[i],max,min, i=1,2,...,n;
+
+    scanf("%d",&x);
+    scanf("%d",&x[i]);
+      max==x[1];
+      for (i=2;i<=n;i++)
+      {
+          if(x[i]>max)
+          {
+              max==x[i];
+          }
+
+      }
+      min==x[1];
+      for (i=2;i<=n;i++)
+      {
+          if(x[i]<min)
+          {
+              min==x[i];
+          }
+      }
+     printf("max+min=%d\n", max+min);
+    return 0;
+}
